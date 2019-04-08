209e94b18e496bda86a37296d42fef3b3a52f809
bee bah <beeviolet.3@gmail.com>
2019-04-08 16:00:51 +0700

    Updates

---
+++ precache-manifest.f747eaa547aa945ed655570430e399a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7391151fb10e7cd85f89",
+    "url": "/ywchomework/static/css/main.621f23c9.chunk.css"
+  },
+  {
+    "revision": "7391151fb10e7cd85f89",
+    "url": "/ywchomework/static/js/main.a8bacb2b.chunk.js"
+  },
+  {
+    "revision": "6e9dc68ea42e6aa755ae",
+    "url": "/ywchomework/static/js/runtime~main.c87e7076.js"
+  },
+  {
+    "revision": "505d0f641f4f865c37fa",
+    "url": "/ywchomework/static/css/2.c94df800.chunk.css"
+  },
+  {
+    "revision": "505d0f641f4f865c37fa",
+    "url": "/ywchomework/static/js/2.59b4a946.chunk.js"
+  },
+  {
+    "revision": "232f3cdc1552b784d2d39ddad1fca771",
+    "url": "/ywchomework/static/media/logo.232f3cdc.png"
+  },
+  {
+    "revision": "3a9b36f81a74c94467b66ae1a3095545",
+    "url": "/ywchomework/index.html"
+  }
+];
